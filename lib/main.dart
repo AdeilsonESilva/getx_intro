6e89659eae116084f0dbd1768f4ff1e38a1150fc
Adeilson Silva <adeilson.silva@brainweb.com.br>
2022-09-23 20:16:41 -0300

    Obx to object

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:getx_intro/user_controller.dart';
 import 'package:getx_intro/value_controller.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MyApp2());
 }
 
 class MyApp extends StatelessWidget {
@@ -59,6 +60,120 @@ class HomePage extends StatelessWidget {
                       child: const Text('Confirmar'),
                     ),
             ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyApp2 extends StatelessWidget {
+  const MyApp2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage2(),
+    );
+  }
+}
+
+class HomePage2 extends StatelessWidget {
+  HomePage2({Key? key}) : super(key: key);
+
+  final nameController = TextEditingController();
+  final ageController = TextEditingController();
+
+  TextStyle commonStyle() => const TextStyle(
+        fontSize: 17,
+        fontWeight: FontWeight.w500,
+      );
+
+  final userController = UserController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Obx(
+              () => Text(
+                'Nome: ${userController.user.value.name}',
+                style: commonStyle(),
+              ),
+            ),
+            Obx(
+              () => Text(
+                'idade: ${userController.user.value.age}',
+                style: commonStyle(),
+              ),
+            ),
+
+            const Divider(
+              thickness: 1.5,
+              color: Colors.blue,
+              height: 20,
+            ),
+
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                // Campo de nome
+                Expanded(
+                  child: TextField(
+                    controller: nameController,
+                    decoration: const InputDecoration(
+                      labelText: 'Nome',
+                    ),
+                  ),
+                ),
+
+                // Botão para salvar o nome
+                ElevatedButton(
+                  onPressed: () {
+                    userController.setUserName(nameController.text);
+                  },
+                  child: const Text('Salvar'),
+                ),
+              ],
+            ),
+
+            // Espaçamento
+            const SizedBox(height: 10),
+
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                // Campo de idade
+                Expanded(
+                  child: TextField(
+                    controller: ageController,
+                    decoration: const InputDecoration(
+                      labelText: 'Idade',
+                    ),
+                  ),
+                ),
+
+                // Botão para salvar a idade
+                ElevatedButton(
+                  onPressed: () {
+                    userController.setUserAge(int.parse(ageController.text));
+                  },
+                  child: const Text('Salvar'),
+                ),
+              ],
+            ),
+
+            // Espaçamento
+            const SizedBox(height: 10),
           ],
         ),
       ),
