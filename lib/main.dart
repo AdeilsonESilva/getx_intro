03d1b41cad6558285525071f57c40acd971a04f4
Adeilson Silva <adeilson.silva@brainweb.com.br>
2022-09-23 21:00:32 -0300

    dependency injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:get/get.dart';
 import 'package:getx_intro/user_controller.dart';
 import 'package:getx_intro/value_controller.dart';
 
 void main() {
-  runApp(const MyApp2());
+  // Get.put<UserController>(UserController());
+  Get.lazyPut<UserController>(() => UserController());
+
+  runApp(const MyApp3());
 }
 
 class MyApp extends StatelessWidget {
@@ -174,6 +177,153 @@ class HomePage2 extends StatelessWidget {
 
             // Espaçamento
             const SizedBox(height: 10),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyApp3 extends StatelessWidget {
+  const MyApp3({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage3(),
+    );
+  }
+}
+
+class HomePage3 extends StatelessWidget {
+  HomePage3({Key? key}) : super(key: key);
+
+  final nameController = TextEditingController();
+  final ageController = TextEditingController();
+
+  final UserController userController = Get.find();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                // Campo de nome
+                Expanded(
+                  child: TextField(
+                    controller: nameController,
+                    decoration: const InputDecoration(
+                      labelText: 'Nome',
+                    ),
+                  ),
+                ),
+
+                // Botão para salvar o nome
+                ElevatedButton(
+                  onPressed: () {
+                    userController.setUserName(nameController.text);
+                  },
+                  child: const Text('Salvar'),
+                ),
+              ],
+            ),
+
+            // Espaçamento
+            const SizedBox(height: 10),
+
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                // Campo de idade
+                Expanded(
+                  child: TextField(
+                    controller: ageController,
+                    decoration: const InputDecoration(
+                      labelText: 'Idade',
+                    ),
+                  ),
+                ),
+
+                // Botão para salvar a idade
+                ElevatedButton(
+                  onPressed: () {
+                    userController.setUserAge(int.parse(ageController.text));
+                  },
+                  child: const Text('Salvar'),
+                ),
+              ],
+            ),
+
+            // Espaçamento
+            const SizedBox(height: 10),
+
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return DataScreen();
+                    },
+                  ),
+                );
+              },
+              child: const Text('Tela de dados'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class DataScreen extends StatelessWidget {
+  DataScreen({
+    Key? key,
+  }) : super(key: key);
+
+  TextStyle commonStyle() => const TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.w700,
+      );
+
+  final userController = Get.find<UserController>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Dados'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            // Apresentação do nome
+            Obx(
+              () => Text(
+                'Nome: ${userController.user.value.name}',
+                style: commonStyle(),
+              ),
+            ),
+
+            // Apresentação da idade
+            Obx(
+              () => Text(
+                'idade: ${userController.user.value.age}',
+                style: commonStyle(),
+              ),
+            ),
           ],
         ),
       ),
